63a6340b1083450911d33849cbd97d2251eccc92
René Brandenburg <renebrandenburg@gmail.com>
2020-02-25 22:13:50 +0100

    Create todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check Off specific Todos by Clicken 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // Click on x to delete Todo 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     })
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+    //  grabbing new todo text from input 
+       var todoText = $(this).val();
+       $(this).val("");
+    //    create a new li and add to ul 
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
